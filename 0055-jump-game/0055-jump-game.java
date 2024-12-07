f87e833200d33242df3f8ad1ae4d0ebcd3ae76f9
Manvaditya <115174973+JadeAdi27@users.noreply.github.com>
2024-12-07 19:17:19 +0530

    Time: 1 ms (100%), Space: 46.2 MB (7.05%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int end=nums.length-1;
+        for(int i=end-1;i>=0;i--){
+            if(i+nums[i]>=end){
+                end=i;
+            }
+        }
+        if(end==0){
+            return true;
+        }
+        return false;
+    }
+}
